5a148f9cfbb4eadbc9c71e9212fdad87a281ca88
Eytan Moudahi <moudahi.e@gmail.com>
2014-01-25 15:37:31 -0500

    Create project.

---
+++ seconddate/seconddate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  seconddate
+//
+//  Created by Eytan Moudahi on 1/25/2014.
+//  Copyright (c) 2014 WTLP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDAppDelegate class]));
+    }
+}
---
+++ seconddate/seconddateTests/seconddateTests.m
@@ -0,0 +1,34 @@
+//
+//  seconddateTests.m
+//  seconddateTests
+//
+//  Created by Eytan Moudahi on 1/25/2014.
+//  Copyright (c) 2014 WTLP. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface seconddateTests : XCTestCase
+
+@end
+
+@implementation seconddateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
